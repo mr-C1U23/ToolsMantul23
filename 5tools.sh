35efd22b8c06f2c6c57837d9961864da3990dc6e
Mr-C1U23 <65049629+mr-C1U23@users.noreply.github.com>
2020-05-10 12:47:13 +0800

    Add files via upload

---
+++ 5tools.sh
@@ -0,0 +1,101 @@
+#!\bin\bash
+
+clear 
+toilet -f slant --gay "T00LS"
+date | lolcat
+echo		
+echo "\033[1;31m"					
+echo "============================================="
+echo "\033[1;31m"
+echo "\033[5;35m Author     : mr-C1U23"
+echo "\033[5;35m Github     : https://github.com/mr-C1U23"
+echo "\033[5;35m Instagram  : @mhmmd.yadz__"
+echo "\033[1;31m"
+echo "============================================="	    						
+echo "\033[1;37m"
+echo "pilih menu tools!"
+echo 
+echo "\033[0;37m [1] HacK FB"
+echo "\033[0;37m [2] Hack IG"
+echo "\033[0;37m [3] Attack Hp"
+echo "\033[0;37m [4] Spam SmS"
+echo "\033[0;37m [5] Ddos"
+echo "\033[0;37m [6] Install bahan"
+echo "\033[0;37m [x] Exit"   
+echo
+read -p "[?] Pilih : " pil;
+# Batas gan
+if [ $pil = 1 ]
+then   
+sleep 5
+echo "\033[1;35m Sedang mengalihkan....."
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+cd hack-facebook-teman
+python2 hack-fb.py
+fi 					
+# Batas gan
+if [ $pil = 2 ]
+then
+echo "\033[1;35m Sedang mengalihkan....."
+sleep 5
+git clone https://github.com/DarkCurut08/DarkCurut08
+cd DarkCurut08
+sh Moreno77.sh
+fi					
+# Batas gan
+if [ $pil = 3 ]
+then
+echo "\033[1;35m Sedang mengalihkan....."
+sleep 5
+git clone https://github.com/R133F/Trojans
+cd Trojanspython2 trojans.py
+
+fi					
+# Batas gan
+if [ $pil = 4 ]
+then
+echo "\033[1;35m Sedang mengalihkan....."
+sleep 5
+git clone https://github.com/Fukur0-3XP/SpamSms
+cd SpamSms
+python2 Spam.py
+fi				
+# Batas gan
+if [ $pil = 5 ]
+then
+echo "\033[1;35m Sedang mengalihkan....."
+sleep 5
+git clone https://github.com/zlucifer/lucita_ddos
+cd lucita_ddos
+python pukul.py
+fi
+# Batas gan
+if [ $pil = 6 ]
+then
+echo "\033[5;31m MEMULAI PENGINSTALAN....."
+sleep 5
+apt update && apt upgrade
+pip2 install --upgrade pipi
+pip2 install requests
+pip2 install mechanize
+pkg install git -y
+pkg install nano -y
+pkg install figlet -y
+pkg install python2 -y
+pip2 install lolcat -y
+pkg install cowsay -y
+pkg install update -y
+pkg install git python2 -y
+sleep 5
+echo "\033[5;31m PENGINSTALAN SELESAI....."
+fi
+# Batas gan
+if [ $pil = x ]
+then
+sleep 2
+echo "\033[1;36m TERIMAKASIH TELAH MENGGUNAKAN TOOLS INI"
+exit
+fi
+
+
+
